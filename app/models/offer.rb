84677dfc6d75a24786d3c0b868ff08f12195f317
Theo Avoyne <tavoyne@gmail.com>
2018-02-28 09:01:51 -0500

    styling

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,7 +4,7 @@ class Offer < ApplicationRecord
 
   validates :user_id, presence: true
   validates :product_id, presence: true, uniqueness: {scope: :user_id}
-  validates :description, length: { maximum: 500, message: "should be less than 500 characters" }
+  validates :description, length: { maximum: 500, message: "Description should be less than 500 characters" }
   validates :state, inclusion: { in: ["pending", "declined", "accepted"], message: "%{value} is not a valid value" }
-  validates :price, numericality: true
+  validates :price, format: { with: /\A\d*\z/, message: "Price only allows numbers" }
 end
