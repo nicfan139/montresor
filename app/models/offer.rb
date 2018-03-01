9fd7ebeff0d1e7ec1bd94eb95e6af781ae21445a
nf139 <35077761+nicfan139@users.noreply.github.com>
2018-03-01 12:37:03 -0500

    updated validation for price where input < 99999

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -6,5 +6,5 @@ class Offer < ApplicationRecord
   validates :product_id, presence: true, uniqueness: {scope: :user_id}
   validates :description, length: { maximum: 500, message: "Description should be less than 500 characters" }
   validates :state, inclusion: { in: ["pending", "declined", "accepted"], message: "%{value} is not a valid value" }
-  validates :price, format: { with: /\A\d*\z/, message: "Price only allows numbers" }
+  validates :price, presence: true, numericality: { less_than_or_equal_to: 99999 }
 end
