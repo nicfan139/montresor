ab02b98b0eeadf46cfd6cdd0bee898413a653ba1
Olivier Riccini <olivier-webdev@hotmail.com>
2018-03-02 16:59:00 -0500

    fixed

--- app/javascript/shared/_counter.js
+++ app/javascript/shared/_counter.js
@@ -1,7 +1,9 @@
 const productDescription = document.getElementById('product_description');
 const offerDescription = document.getElementById('offer_description');
+const comment = document.getElementById('comment_content');
 const countProduct = document.getElementById('count-product');
 const countOffer = document.getElementById('count-offer');
+const countComment = document.getElementById('count-comment');
 
 function count(event, count, counter, textarea) {
   let c = count - textarea.value.split('').length;
@@ -25,5 +27,11 @@ if (productDescription) {
 if (offerDescription) {
   offerDescription.addEventListener('keyup', function(e) {
     count(e, 500, countOffer, offerDescription);
+  });
+}
+
+if (comment) {
+  comment.addEventListener('keyup', function(e) {
+    count(e, 500, countComment, comment);
   });
 }
