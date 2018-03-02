e5ee3ac36c18f9f7ba6270a737043ba51a42286e
Olivier Riccini <olivier-webdev@hotmail.com>
2018-03-02 14:57:46 -0500

    fadeOut time changing

--- app/javascript/shared/_arrowUp.js
+++ app/javascript/shared/_arrowUp.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   //Check to see if the window is top if not then display button
   $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
+    if ($(this).scrollTop() > 500) {
       $('.scrollToTop').fadeIn();
     } else {
       $('.scrollToTop').fadeOut();
